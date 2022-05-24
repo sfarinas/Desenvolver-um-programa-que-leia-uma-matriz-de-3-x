2bd1c30081216f36e0eb95432437816a7a37e7e3
sfarinas <sfarinas@gmail.com>
2022-05-23 21:53:44 -0300

    calculadora

---
+++ calculadora.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <isdigit.io>
+using namespace std;
+
+int main(){
+
+    float num1, num2;
+
+    //Recebendo os numeros
+    cout << "Primeiro numero: ";
+    cin >> num1;
+
+    cout << "Segundo numero: ";
+    cin >> num2;
+
+    cout << "Soma          : " << num1 + num2 << endl;
+    cout << "Subtracao     : " << num1 - num2 << endl;
+    cout << "multiclicacao : " << num1 * num2 << endl;
+    cout << "Divisao       : " << num1 / num2 << endl;
+
+}
+
